5480092dad45598e49d8ab7c1e0dfa60e6e34f71
sangwooeom <cdm19720@gmail.com>
2024-10-09 21:17:50 +0900

    네비게이션 수정 완료

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,18 +1,20 @@
-'use client'
-
 import Link from 'next/link';
 import styles from './index.module.scss';
 import cn from 'classnames';
+import Image from 'next/image';
+import { logo } from '@/images';
 
 export default function Navigation() {
     return (
         <nav className={cn(styles.navigation)}>
             <div className={cn(styles.container)}>
-                <i className={styles.logo}></i>
+                <Link href="/">
+                    <Image className={cn(styles.logo)} src={logo} alt="로고" />
+                </Link>
                 <div className={styles.btn}>
                     <button>스터디 생성하기</button>
-                    <Link href="/">스터디 찾기</Link>
-                    <Link href="/">멤버 찾기</Link>
+                    <Link href="/studies">스터디 찾기</Link>
+                    <Link href="/members">멤버 찾기</Link>
                     <Link href="/login">로그인</Link>
                 </div>
             </div>
--- src/images/index.ts
+++ src/images/index.ts
@@ -2,10 +2,12 @@ import googleImg from './google.svg';
 import kakaoImg from './kakao.svg';
 import visibilityOffIcon from './ic_visibility_off.svg';
 import visibilityOnIcon from './ic_visibility_on.svg';
+import logo from './logo.svg';
 
 export {
     googleImg,
     kakaoImg,
     visibilityOffIcon,
     visibilityOnIcon,
+    logo,
 }
